e9a355cabd12f75159336d5bc6868fb83443a887
Tung Do <tdo@haverford.edu>
2020-05-06 20:23:21 -0400

    done with game

--- scripts.js
+++ scripts.js
@@ -1,22 +1,43 @@
-let red=Math.floor(Math.random() * 256);
-let green=Math.floor(Math.random() * 256);
-let blue=Math.floor(Math.random() * 256);
+let red = Math.floor(Math.random() * 256);
+let green = Math.floor(Math.random() * 256);
+let blue = Math.floor(Math.random() * 256);
+let score=0;
 
-
-document.appendChild("Aaaaaa");
-document.getElementById("sth").style.backgroundColor="red";
-let select=document.querySelector('#options');
-let result=document.querySelector('.result');
+document.getElementById("sth").style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+let select = document.querySelector('#options');
+let result = document.querySelector('.result');
 select.addEventListener("change",runGame);
 function runGame() { 
-    if ((red=Math.max(red,green,blue) && select.value="red") || 
-    (green=Math.max(red,green,blue) && select.value="green") ||
-    (blue=Math.max(red,green,blue) && select.value="blue")) { 
-        result.textContent="Congratulations! You got this right.";
+    if ((red===Math.max(red,green,blue) && select.value.localeCompare("red")===0) || (green===Math.max(red,green,blue) && select.value.localeCompare("green")===0)
+     || (blue===Math.max(red,green,blue) && select.value.localeCompare("blue")===0)) { 
+        score+=1;
+        result.textContent="Congratulations! You got this right. Your score is " + score;
+        select.value="default";
+        red = Math.floor(Math.random() * 256);
+        green = Math.floor(Math.random() * 256);
+        blue = Math.floor(Math.random() * 256);
+        document.getElementById("sth").style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        select.addEventListener("change",runGame);
     }
     else { 
-        result.textContent="Oops. You got it wrong.";
+        result.textContent="Oops. You got it wrong. Your score is " + score;
+        select.value="default";
+        button=document.createElement("button"); 
+        button.textContent="Try again!";
+        button.id="button-1"
+        document.body.appendChild(button);
+        button.onclick=replay;
     }
+}
+function replay() { 
+    red = Math.floor(Math.random() * 256);
+    green = Math.floor(Math.random() * 256);
+    blue = Math.floor(Math.random() * 256);
+    score=0;
+    document.getElementById("sth").style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    result.textContent="";
+    document.getElementById("button-1").remove();
+    runGame;
 }
 
 
--- test.js
+++
@@ -1,1 +0,0 @@
-document.appendChild("lollll")
